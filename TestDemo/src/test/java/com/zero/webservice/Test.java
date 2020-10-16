3f453bda99b5bc8a45c8f37154e2ec8efc562111
worldgu <15893851179@163.com>
2020-10-16 23:59:07 +0800

    测试代码

---
+++ TestDemo/src/test/java/com/zero/webservice/Test.java
@@ -0,0 +1,27 @@
+package com.zero.webservice;/**
+ * Created by 86158 on 2020/4/26  13:18
+ */
+
+/**
+ *@ClassName Test
+ *@Description TODO
+ *@Author 86158
+ *@Date 2020/4/26 13:18 
+ *@V|ersion 1.0 */
+
+public class Test {
+
+
+//    @Test
+//    public void testObejct(){
+//
+//    }
+
+    public static void main(String[] args) {
+        Object obj = null;
+        System.out.println("11>>>> " + (obj==null));
+
+        Object obj1 = new Object();
+        System.out.println("11>>>> " + (obj1!=null));
+    }
+}
---
+++ TestDemo/src/test/java/com/zero/webservice/TestTest.java
@@ -0,0 +1,10 @@
+package com.zero.webservice;
+
+//import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by 86158 on 2020/4/26  13:19
+ */
+class TestTest {
+
+}
